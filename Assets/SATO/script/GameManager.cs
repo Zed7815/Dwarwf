cbc99b790f13bf04a5514df93db730d899df521c
2257021 <2257021@25HS21>
2026-06-09 19:43:17 +0900

    botan

---
+++ Assets/SATO/script/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState
+    {
+        Edit,
+        Play
+    }
+
+    public GameState currentState = GameState.Edit;
+    public PlayerController player;
+
+    public GameObject startButton;
+    public GameObject resetButton;
+
+    void Start()
+    {
+        SetUI();
+    }
+
+    public void StartGame()
+    {
+        currentState = GameState.Play;
+        player.StartMove();
+        SetUI();
+    }
+
+    public void ResetGame()
+    {
+        currentState = GameState.Edit;
+        player.ResetPosition();
+        SetUI();
+    }
+
+    void SetUI()
+    {
+        if (currentState == GameState.Edit)
+        {
+            startButton.SetActive(true); // スタートボタン表示
+            resetButton.SetActive(false); // リセットボタンを非表示
+        }
+
+        else
+        {
+            startButton.SetActive(false); // スタートボタンの非表示
+            resetButton.SetActive(true); // リセットボタンを表示
+        }
+    }
+}
---
+++ Assets/SATO/script/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameManager gameManager; // GameManagerを参照
+    public Player_walk playerWalk;  // PlayerWalkを参照 
+
+    private Vector3 startPosition; // 最初の位置を覚えておく変数
+
+    void Start()
+    {
+        // ゲーム開始時の位置を保存しておく
+        startPosition = transform.position;
+    }
+
+    public void StartMove()
+    {
+        playerWalk.StateChange(1); // straight
+    }
+
+    public void StopMove()
+    {
+        playerWalk.StateChange(0); // idol
+    }
+
+
+    // 初期位置に戻るための関数
+    public void ResetPosition()
+    {
+        transform.position = startPosition;
+        StopMove();
+        // 物理演算（Rigidbody2D）を使っている場合、速度も0
+        // GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
