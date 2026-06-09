a45db4bd79511f10a6adb01c787ae6748feb72ef
2257021 <2257021@25HS21>
2026-06-09 19:55:59 +0900

    最新

--- Assets/SATO/script/Player_walk.cs
+++ Assets/SATO/script/Player_walk.cs
@@ -11,7 +11,7 @@ public class Player_walk : MonoBehaviour
 
     [Header("プレイヤーの数値")]
     public float PlayerSpeed = 5.0f;
-
+    public int direction = 1;
     private moveState state = moveState.idol;
 
     void Update()
@@ -38,7 +38,7 @@ public class Player_walk : MonoBehaviour
 
     void walk()
     {
-        transform.Translate(Vector2.right * PlayerSpeed * Time.deltaTime);
+        transform.Translate(Vector2.right * PlayerSpeed * Time.deltaTime* direction);
     }
 
     public void StateChange(int n)
@@ -58,4 +58,5 @@ public class Player_walk : MonoBehaviour
                 break;
         }
     }
+    void OnCollisionEnter2D(Collision2D collision) { if (collision.gameObject.CompareTag("wall")) { direction *= -1; Vector3 scale = transform.localScale; scale.x = -1; transform.localScale = scale; } }
 }
