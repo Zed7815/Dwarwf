f144e8f65c9f6592023e98b1bdfcab232d7f3c1b
蒼惟 平松 <2257030@ecc.ac.jp>
2026-06-09 19:37:24 +0900

    バグなし

--- Assets/HIRAMATSU/scene/Player.cs
+++ Assets/HIRAMATSU/scene/Player.cs
@@ -1,73 +1,36 @@
 using UnityEngine;
-public enum moveState
-{
-    idol,
-    straight,
-    jump,
 
-}
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb;
     [Header("プレイヤーの数値")]
     public float PlayerSpeed = 5.0f;
     public float PlayerJumpPower=5.0f;
     public int direction = 1;
-    moveState state = moveState.straight;
-    // Update is called once per frame
+       // Update is called once per frame
+    private void Start()
+    {
+        
+    }
     void Update()
     {
-        switch (state)
-        {
-            case moveState.idol:
-                stay();
-                break;
-
-            case moveState.straight:
-                walk();
-                break;
-
-            case moveState.jump:
-                jump();
-                break;
-
-        }
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            StateChange(2);
-        }
+        Walk();
     }
     void stay()
     {
 
     }
-    void walk()
+    void Walk()
     {
         transform.Translate(Vector2.right * PlayerSpeed * Time.deltaTime* direction);
     }
     void jump()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, PlayerJumpPower);
+                
     }
 
 
 
-    public void StateChange(int  n)
-    {
-        switch (n)
-        {
-            case 0:
-                state = moveState.idol;
-                break;
-
-            case 1:
-                state = moveState.straight;
-                break;
-            case 2:
-                state = moveState.jump;
-                break;
-        }
-    }
+   
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("wall"))
