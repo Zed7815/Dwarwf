8dcfe9197f290c411e80001757d8b10df4adcccf
蒼惟 平松 <2257030@ecc.ac.jp>
2026-06-30 16:21:29 +0900

    a

--- Assets/Main/script/New Folder/main_SmoothZoom2D.cs
+++ Assets/Main/script/New Folder/main_SmoothZoom2D.cs
@@ -1,35 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class SmoothZoom2D : MonoBehaviour
+public class MouseZoom2D : MonoBehaviour
 {
     public Camera cam;
-    public float zoomSpeed = 5f;
-    public float smoothSpeed = 5f;
+    public float zoomSpeed = 0.01f;
     public float minZoom = 2f;
     public float maxZoom = 10f;
-
-    float targetZoom;
-
-    void Start()
-    {
-        targetZoom = cam.orthographicSize;
-    }
-
     void Update()
     {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        float scroll = Mouse.current.scroll.ReadValue().y;
 
-        if (scroll != 0f)
-        {
-            targetZoom -= scroll * zoomSpeed;
-            targetZoom = Mathf.Clamp(targetZoom, minZoom, maxZoom);
-        }
+        if (scroll == 0) return;
 
-        cam.orthographicSize = Mathf.Lerp(
-            cam.orthographicSize,
-            targetZoom,
-            Time.deltaTime * smoothSpeed
-        );
+        // ズーム前のマウス位置のワールド座標
+        Vector3 mouseWorldBefore = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        mouseWorldBefore.z = 0;
+
+        // ズーム
+        cam.orthographicSize -= scroll * zoomSpeed;
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minZoom, maxZoom);
+
+        // ズーム後のマウス位置のワールド座標
+        Vector3 mouseWorldAfter = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        mouseWorldAfter.z = 0;
+
+        // 差分だけカメラを移動
+        cam.transform.position += mouseWorldBefore - mouseWorldAfter;
+                
+    }
+
+    public float minX = -10f;
+    public float maxX = 10f;
+    public float minY = -5f;
+    public float maxY = 5f;
+
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
     }
 }
-
