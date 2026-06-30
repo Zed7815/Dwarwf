b4e3fcc41cd3d743a46260f6bc202f10d0663e52
蒼惟 平松 <2257030@ecc.ac.jp>
2026-06-30 18:50:39 +0900

    AD

---
+++ Assets/Main/script/New Folder/main_chu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TutorialMove : MonoBehaviour
+{
+    public main_GameManager gameManager;
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 1f;
+    public float moveTime = 3f; // A→Bにかかる秒数
+    bool waiting = false;
+    SpriteRenderer sr;
+    void Start()
+    {
+        transform.position = pointA.position;
+        sr = GetComponent<SpriteRenderer>();
+        Color color = sr.color;
+    }
+
+    void Update()
+    {
+        if (waiting) return;
+        if (gameManager.blockManager != null && !gameManager.blockManager.IsAllBlocksPlaced())
+        {
+            Color resetColor = sr.color;
+            resetColor.a = 0.5f;
+            sr.color = resetColor;
+            transform.position = Vector3.MoveTowards(
+            transform.position,
+            pointB.position,
+            speed * Time.deltaTime
+        );
+
+            if (Vector3.Distance(transform.position, pointB.position) < 0.01f)
+            {
+                StartCoroutine(ResetPosition());
+            }
+        }
+        else
+        {
+            Color resetColor = sr.color;
+            resetColor.a = 0f;
+            sr.color = resetColor;
+        }
+    }
+
+    IEnumerator ResetPosition()
+    {
+        waiting = true;
+
+            yield return new WaitForSeconds(1f);
+
+
+        transform.position = pointA.position;
+        waiting = false;
+    }
+}
---
+++ Assets/Main/script/New Folder/main_chu2.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main_chu2 : MonoBehaviour
+{
+    public main_GameManager gameManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    bool waiting = false;
+    SpriteRenderer sr;
+    public Transform pointA;
+    public Transform pointB;
+    public float moveTime = 3f; // A→Bにかかる秒数
+    public bool a;
+
+   void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        Color color = sr.color;
+    }
+    void Update()
+    {
+        if(a)
+        {
+            Color resetColor = sr.color;
+            resetColor.a = 1f;
+            sr.color = resetColor;
+            float t = Mathf.PingPong(Time.time / moveTime, 1f);
+
+            transform.position = Vector3.Lerp(
+                pointA.position,
+                pointB.position,
+                t
+            );
+        }
+        else
+        {
+            Color resetColor = sr.color;
+            resetColor.a = 0f;
+            sr.color = resetColor;
+        }
+        StartCoroutine(U());
+    }
+        IEnumerator U()
+        {
+            if (gameManager.blockManager != null && !gameManager.blockManager.IsAllBlocksPlaced())
+            {
+                Color resetColor = sr.color;
+                resetColor.a = 0f;
+                sr.color = resetColor;
+            yield return new WaitForSeconds(3f);
+            a = true;
+        }
+            else
+            {
+            a = false;
+                Color resetColor = sr.color;
+                resetColor.a = 1f;
+                sr.color = resetColor;
+                float t = Mathf.PingPong(Time.time / moveTime, 1f);
+
+                transform.position = Vector3.Lerp(
+                    pointA.position,
+                    pointB.position,
+                    t
+                );
+            }
+            
+        }
+    }
