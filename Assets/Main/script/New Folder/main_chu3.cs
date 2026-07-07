9ac41918891fd241ed6509fb4f0332d9dd6706d1
蒼惟 平松 <2257030@ecc.ac.jp>
2026-07-07 14:09:47 +0900

    チュートリアル完成

---
+++ Assets/Main/script/New Folder/main_chu3.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main_chu3 : MonoBehaviour
+{
+    public main_GameManager gameManager;
+    public float rotateTime = 1f;
+    public GameObject b;
+    private bool rotating = false;
+
+    private void Update()
+    {
+        if (!rotating &&
+            gameManager.blockManager != null &&
+            gameManager.blockManager.IsAllBlocksPlaced())
+        {
+            StartCoroutine(RotateRoutine());
+        }
+    }
+
+    IEnumerator RotateRoutine()
+    {
+        rotating = true;
+
+        Quaternion startRot = transform.rotation;
+        Quaternion endRot = startRot * Quaternion.Euler(-90, 0, 0);
+
+        float t = 0;
+
+        while (t < rotateTime)
+        {
+            t += Time.deltaTime;
+
+            transform.rotation = Quaternion.Lerp(
+                startRot,
+                endRot,
+                t / rotateTime);
+
+            yield return null;
+        }
+        b.transform.position = new Vector3(-5, -4.5f, 0);
+        transform.rotation = endRot;
+        
+    }
+}
