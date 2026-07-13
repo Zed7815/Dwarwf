fce5419874ea36fe2dbcc5e760dbd5eb682abbb1
蒼惟 平松 <2257030@ecc.ac.jp>
2026-07-13 18:29:21 +0900

    happa

---
+++ Assets/Main/script/New Folder/tirasi.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tirasi : MonoBehaviour
+{
+    public GameObject prefab;// trinのついてる葉っぱのPrefab
+
+    public float minX = -8f;
+    public float maxX = 8f;
+    public float spawnY = 5f;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            float wait = Random.Range(0.4f, 0.2f);
+            yield return new WaitForSeconds(wait);
+
+            float randomX = Random.Range(minX, maxX);
+            Vector3 spawnPos = new Vector3(randomX, spawnY, 0);
+
+            Instantiate(prefab, spawnPos, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Main/script/New Folder/trin.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+public class trin : MonoBehaviour
+{
+
+    public float minFallSpeed = 1.5f;
+    public float maxFallSpeed = 3.0f;
+
+    public float minSwaySpeed = 1.0f;
+    public float maxSwaySpeed = 3.0f;
+
+    public float minSwayAmount = 0.2f;
+    public float maxSwayAmount = 0.8f;
+
+    public float minRotateSpeed = 20f;
+    public float maxRotateSpeed = 80f;
+
+    private float fallSpeed;
+    private float swaySpeed;
+    private float swayAmount;
+    private float rotateSpeed;
+
+    private float startX;
+    private float offset;
+
+    void Start()
+    {
+        startX = transform.position.x;
+        StartCoroutine(die());
+        // ランダムに設定
+        fallSpeed = Random.Range(minFallSpeed, maxFallSpeed);
+        swaySpeed = Random.Range(minSwaySpeed, maxSwaySpeed);
+        swayAmount = Random.Range(minSwayAmount, maxSwayAmount);
+        rotateSpeed = Random.Range(minRotateSpeed, maxRotateSpeed);
+
+        // 揺れ始めるタイミングをランダムにする
+        offset = Random.Range(0f, Mathf.PI * 2);
+    }
+
+    void Update()
+    {
+        // 左右にゆらゆら
+        float x = startX + Mathf.Sin(Time.time * swaySpeed + offset) * swayAmount;
+
+        // 下へ落ちる
+        float y = transform.position.y - fallSpeed * Time.deltaTime;
+
+        transform.position = new Vector3(x, y, transform.position.z);
+
+        // ゆっくり回転
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+    }
+    IEnumerator die()
+    {
+        yield return new WaitForSeconds(7f);
+        Destroy(gameObject);
+    }
+}
