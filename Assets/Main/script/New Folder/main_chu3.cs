cb6bb7d8ace52bf06008caeae8a55e1d40482ec6
蒼惟 平松 <2257030@ecc.ac.jp>
2026-07-23 18:07:28 +0900

    main_chu3

--- Assets/Main/script/New Folder/main_chu3.cs
+++ Assets/Main/script/New Folder/main_chu3.cs
@@ -6,6 +6,7 @@ public class main_chu3 : MonoBehaviour
     public main_GameManager gameManager;
     public float rotateTime = 1f;
     public GameObject b;
+    public GameObject c; //もう一枚の看板
     private bool rotating = false;
 
     private void Update()
@@ -15,6 +16,7 @@ public class main_chu3 : MonoBehaviour
             gameManager.blockManager.IsAllBlocksPlaced())
         {
             StartCoroutine(RotateRoutine());
+            StartCoroutine(RotateRoutine2());
         }
     }
 
@@ -41,5 +43,29 @@ public class main_chu3 : MonoBehaviour
         //b.transform.position = new Vector3(-5, -4.5f, 0);
         transform.rotation = endRot;
         
+    }
+    IEnumerator RotateRoutine2()
+    {
+        rotating = true;
+
+        Quaternion startRot = c.transform.rotation;
+        Quaternion endRot = startRot * Quaternion.Euler(-90, 0, 0);
+
+        float t = 0;
+
+        while (t < rotateTime)
+        {
+            t += Time.deltaTime;
+
+            c.transform.rotation = Quaternion.Lerp(
+                startRot,
+                endRot,
+                t / rotateTime);
+
+            yield return null;
+        }
+        //b.transform.position = new Vector3(-5, -4.5f, 0);
+        c.transform.rotation = endRot;
+
     }
 }
---
+++ Assets/Main/script/New Folder/main_chu4.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+public class main_chu4 : MonoBehaviour
+{
+    public main_GameManager gameManager;
+    public float rotateTime = 1f;
+    public GameObject b;
+    private bool rotating = false;
+    IEnumerator RotateRoutine()
+    {
+        rotating = true;
+
+        Quaternion startRot = transform.rotation;
+        Quaternion endRot = startRot * Quaternion.Euler(-90, 0, 0);
+
+        float t = 0;
+
+        while (t < rotateTime)
+        {
+            t += Time.deltaTime;
+
+            transform.rotation = Quaternion.Lerp(
+                startRot,
+                endRot,
+                t / rotateTime);
+
+            yield return null;
+        }
+        //b.transform.position = new Vector3(-5, -4.5f, 0);
+        transform.rotation = endRot;
+
+    }
+}
