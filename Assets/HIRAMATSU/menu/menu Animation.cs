f46505140b99b4afb362b16fdfe45f3620719201
蒼惟 平松 <2257030@ecc.ac.jp>
2026-09-25 12:06:31 +0900

    あおい

---
+++ Assets/HIRAMATSU/menu/menu Animation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuAnimation : MonoBehaviour
+{
+    public float startY = 800f;
+    public float targetY = 0f;
+    public float duration = 0.5f;
+
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        targetPosition = transform.localPosition;
+
+        Vector3 startPosition = targetPosition;
+        startPosition.y = startY;
+
+        transform.localPosition = startPosition;
+    }
+
+    public void OnMouseDown()
+    {
+        StartCoroutine(Drop());
+    }
+
+    IEnumerator Drop()
+    {
+        Vector3 startPosition = transform.localPosition;
+
+        float time = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+
+            float t = time / duration;
+            t = Mathf.SmoothStep(0f, 1f, t);
+
+            transform.localPosition =
+                Vector3.Lerp(startPosition, targetPosition, t);
+
+            yield return null;
+        }
+
+        transform.localPosition = targetPosition;
+    }
+}
